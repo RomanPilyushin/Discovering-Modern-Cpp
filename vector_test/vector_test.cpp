8ce2e13754ffbe8a0a3c625b9026aadd44ea62bc
Roman <rpilyushin@gmail.com>
2023-09-13 18:16:17 +0300

    Edit vector_test

--- vector_test/vector_test.cpp
+++ vector_test/vector_test.cpp
@@ -41,8 +41,7 @@ public:
     int size() const { return my_size; }
     int size() { return my_size; }
 
-    double& operator[](int i) const 
-    {
+    double& operator[](int i) const {
         assert(i >= 0 && i < my_size);
         return data[i];
     }
